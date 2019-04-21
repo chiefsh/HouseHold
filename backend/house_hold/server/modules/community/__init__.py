4fc83db5bafc2497bb4f60fb96de15d98aa52b1f
chiefsh <chief_victo@163.com>
2019-04-21 17:30:42 +0800

    添加置顶和上下移接口

--- backend/house_hold/server/modules/community/__init__.py
+++ backend/house_hold/server/modules/community/__init__.py
@@ -1,7 +1,8 @@
-from .community import CommunityAddHandler, CommunityQueryHandler, CommunityDeleteHandler
-
+from .community import CommunityAddHandler, CommunityQueryHandler, CommunityDeleteHandler, CommunityIsTopHandler, CommunitySortHandler
 urls = [
     ("/api/community/add", CommunityAddHandler),
     ("/api/community/query", CommunityQueryHandler),
     ("/api/community/delete", CommunityDeleteHandler),
+    ("/api/community/up", CommunitySortHandler),
+    ("/api/community/top", CommunityIsTopHandler),
 ]
