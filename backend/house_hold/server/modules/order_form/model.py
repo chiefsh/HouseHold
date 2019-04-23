fb297b8d9e9a1a708d081c0b4a0d5d6f884022e7
chiefsh <chief_victo@163.com>
2019-04-23 18:13:44 +0800

    调试

--- backend/house_hold/server/modules/order_form/model.py
+++ backend/house_hold/server/modules/order_form/model.py
@@ -114,7 +114,10 @@ class OrderFormModel(MysqlModel):
         return product
 
     def get_group_order_form_info(self, community_id, category_id, product_id):
-        query = self.session.query(Product)
+        query = self.session.query(Product.product_id, Product.name, Product.category_ids, Product.image_0,
+                                   Product.image_1,
+                                   Product.image_2, Product.image_3, Product.image_4,
+                                   Product.community_id, Product.group_number)
         if product_id is None or not product_id:
             if community_id:
                 query = query.filter(Product.community_id.like("%{}%".format(str(community_id))))
@@ -123,7 +126,10 @@ class OrderFormModel(MysqlModel):
             product_list = query.order_by(
                 Product.is_top.desc(), Product.created_at.desc()
             ).all()
-            product_list = [row2dict(product) for product in product_list] if product_list else []
+            # product_list = [row2dict(product) for product in product_list] if product_list else []
+            product_list = [{"product_id": item[0], "name": item[1], "category_ids": item[2], "image_0": item[3],
+                     "image_1": item[4], "image_2": item[5], "image_3": item[6], "image_4": item[7],
+                     "community_id": item[8], "group_number": item[9]} for item in product_list] if product_list else []
             for product in product_list:
                 self._format_product(product)
 
